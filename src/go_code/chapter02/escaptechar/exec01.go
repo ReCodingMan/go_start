2b5cc452374a3247f6ed7955f6d3384d04ecec59
chengcheng1021 <chengcheng1021@aliyun.com>
2020-08-01 11:07:55 +0800

    编码规范

---
+++ src/go_code/chapter02/escaptechar/exec01.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("姓名\t年龄\t籍贯\n程成\t20\t江苏")
+}
--- src/go_code/chapter02/escaptechar/main.go
+++ src/go_code/chapter02/escaptechar/main.go
@@ -12,4 +12,9 @@ func main() {
 	fmt.Println("nihao\"haha\"")
 	// \r 回车，从当前行的最前面开始输出，覆盖掉以前内容
 	fmt.Println("nihao\rhaha")
+
+	fmt.Println("hehfhehfaflalflalfla",
+	"hehfhehfaflalflalfla",
+	"hehfhehfaflalflalfla",
+	"hehfhehfaflalflalfla")
 }

22c5035c3f8c706d4a188627480a19210d65a359
chengcheng1021 <chengcheng1021@aliyun.com>
2020-08-01 10:26:27 +0800

    转译字符测试

---
+++ src/go_code/chapter02/escaptechar/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("nihao\thaha")
+
+	fmt.Println("nihao\nhaha")
+
+	fmt.Println("nihao\\haha")
+
+	fmt.Println("nihao\"haha\"")
+	// \r 回车，从当前行的最前面开始输出，覆盖掉以前内容
+	fmt.Println("nihao\rhaha")
+}

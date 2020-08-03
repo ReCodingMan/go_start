f99e46d042596e113f77f69946bf5abe289811cc
chengcheng1021 <chengcheng1021@aliyun.com>
2020-08-03 16:17:15 +0800

    基本数据类型转string

---
+++ src/go_code/project02/change/main.go
@@ -0,0 +1,46 @@
+package main
+import (
+	"fmt"
+	_ "unsafe"
+	"strconv"
+)
+
+// 基本类型转string使用
+func main() {
+	
+	var num1 int = 99
+	var num2 float64 = 23.456
+	var b bool = true
+	var mychar byte = 'h'
+	var str string // 空string
+
+	//使用第一种方式 fmt.Sprintf方法
+	str = fmt.Sprintf("%d", num1)
+	fmt.Printf("str type %T str= %v", str, str)
+
+	str = fmt.Sprintf("%f", num2)
+	fmt.Printf("str type %T str= %v", str, str)
+
+	str = fmt.Sprintf("%t", b)
+	fmt.Printf("str type %T str= %q", str, str)
+
+	str = fmt.Sprintf("%c", mychar)
+	fmt.Printf("str type %T str= %q", str, str)
+
+
+	//使用第二种方法 strconv 函数
+	var num3 int = 99
+	var num4 float64 = 23.456
+	var b2 bool = true
+
+	str = strconv.FormatInt(int64(num3), 10)
+	fmt.Printf("str type %T str= %q", str, str)
+
+	//strconv.FormatFloat(num4, 'f', 10, 64)
+	//说明：'f'格式，10：表示小数位保留10位，64：表示这个小数是float64
+	str = strconv.FormatFloat(num4, 'f', 10, 64)
+	fmt.Printf("str type %T str= %q", str, str)
+
+	str = strconv.FormatBool(b2)
+	fmt.Printf("str type %T str= %q", str, str)
+}

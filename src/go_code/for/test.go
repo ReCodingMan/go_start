d3026d97b4b5283fbe2e05f6d77f7a4eb740d87c
chengcheng1021 <chengcheng1021@aliyun.com>
2020-08-04 17:15:14 +0800

    循环测试

---
+++ src/go_code/for/test.go
@@ -0,0 +1,27 @@
+package main
+import (
+	"fmt"
+)
+
+func main() {
+	
+	//第一题
+	var max int = 100
+	var count int
+	var sum int
+
+	for i:=1; i<=max; i++ {
+		if i%9 == 0 {
+			count++
+			sum+=i
+		}
+	}
+
+	fmt.Printf("count=%v sum=%v\n", count, sum)
+
+	//第二题
+	var n int = 6
+	for i:=0; i<=n; i++ {
+		fmt.Printf("%v + %v = %v \n", i, n-i, n)
+	}	
+}

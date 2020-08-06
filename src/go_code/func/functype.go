a7886fe31d4169c36d6c2162fae7a3259cfc06d4
chengcheng1021 <chengcheng1021@aliyun.com>
2020-08-06 17:24:47 +0800

    递归方法和方法返回值定义

--- src/go_code/func/functype.go
+++ src/go_code/func/functype.go
@@ -6,6 +6,23 @@ import (
 //函数类型
 type myFunctype func(int, int) int
 
+//函数是一种类型，在go中，可以作为参数，并且调用
+func myFun2(funvar myFunctype, num1 int, num2 int) int {
+	return funvar(num1, num2)
+}
+
+//求和
+func getSum(n1 int, n2 int) int {
+	return n1 + n2
+}
+
+//支持返回值命名
+func getSumAndSub(n1 int, n2 int) (sum int, sub int){
+	sum = n1 + n2
+	sub = n1 - n2
+	return
+}
+
 func main() {
 
 	//类型不是一个
@@ -18,12 +35,10 @@ func main() {
 
 	fmt.Println("num1=", num1, "num2=", num2)
 
-	//函数是一种类型，在go中，可以作为参数，并且调用
-	func myFun2(funvar myFunctype, num1 int, num2 int) int {
-		return funvar(num1, num2)
-	}
-
 	res3 := myFun2(getSum, 500, 600)
 	fmt.Println("res3=", res3)
 
+	//求和和差
+	a, b := getSumAndSub(1, 2)
+	fmt.Println("a=", a, "b=", b)
 }
--- src/go_code/func/taozi.go
+++ src/go_code/func/taozi.go
@@ -4,13 +4,17 @@ import (
 )
 
 func main() {
-	
+	fmt.Println("第一天桃子数量：", taozi(1))
 }
 
-func taozi(n int) {
+func taozi(n int) int {
+	if n > 10 || n < 1 {
+		fmt.Println("输入天数不对")
+	}
+
 	if n == 10 {
 		return 1
 	} else {
-		
+		return (taozi(n + 1) + 1) * 2
 	}
 }

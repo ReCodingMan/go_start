f111b5cd68494cf0eb08c109d3cc5072aea972e8
chengcheng1021 <chengcheng1021@aliyun.com>
2020-10-28 17:05:56 +0800

    闭包加强版

--- src/go_code/funcdemo07/main.go
+++ src/go_code/funcdemo07/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
 //累加器
@@ -19,4 +20,21 @@ func main() {
 	fmt.Println(f(1)) //11
 	fmt.Println(f(2)) //13
 	fmt.Println(f(3)) //16
+
+	//测试 makeSuffix
+	str1 := makeSuffix(".jpg")
+	fmt.Println(str1("haha.jpg"))
+	fmt.Println(str1("cc"))
+}
+
+
+func makeSuffix(suffix string) func (string) string {
+
+	return func(name string) string {
+		if !strings.HasSuffix(name, suffix) {
+			return name + suffix
+		}
+		return name
+	}
+
 }

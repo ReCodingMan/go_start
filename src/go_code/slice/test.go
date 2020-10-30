874ecabb3fc9b48f061bd16773842d8382ea3691
chengcheng1021 <chengcheng1021@aliyun.com>
2020-10-30 10:53:15 +0800

    切片copy和append

--- src/go_code/slice/test.go
+++ src/go_code/slice/test.go
@@ -10,6 +10,10 @@ func main() {
 
 	// 修改切片
 	slice[0] = 88
+	fmt.Println("slice=", slice) // [88 3]
+	fmt.Println("intArr=", intArr) // [1 88 3 4 5]
+
+	slice = append(slice, 40,50,60)
 	fmt.Println("slice=", slice)
 	fmt.Println("intArr=", intArr)
 
@@ -20,4 +24,11 @@ func main() {
 	for i,v := range slice {
 		fmt.Printf("i=%v, v=%v \n", i, v)
 	}
+
+	var a []int = []int{1,2,3,4,5}
+	b := make([]int, 6, 10)
+	fmt.Println(b)
+	copy(b, a)
+	fmt.Println(b)
+
 }
